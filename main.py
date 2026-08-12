f85fe877f41e2dcfaaf264b381b57fa30b1a4ad9
Maruthi Prasad B P <93729606+PrasadMaruthi@users.noreply.github.com>
2026-08-12 23:29:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -401,7 +401,15 @@ if page == "🏠 Home":
 
     col_logo1, col_logo2, col_logo3 = st.columns([1, 2, 1])
 
-    st.image(image_path, use_column_width=True)
+    with col_logo2:
+
+        try:
+
+            st.image("uas_logo.png", use_column_width=True)
+
+        except:
+
+            pass
 
        
 
